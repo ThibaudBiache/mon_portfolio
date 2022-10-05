64496141860f067fae6ac814a1616e4a16457e9a
chouka <biache.thibaud@gmail.com>
2022-10-05 18:49:48 +0200

    added some style on mail button

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+// Toutes les variables dont j'ai besoin
+const presentationBox = document.querySelector('.presentation-box');
+
+// Les diverses fonctions
 const copyToClipboard = () => {
     navigator.clipboard.writeText("biache.thibaud@gmail.com");
 }
+
+const doSomethingElse = () => {
+    const button = document.querySelector('.mail-btn');
+    console.log(button)
+    button.textContent = "Copié !";
+    button.classList.add('mail-btn-clicked');
+    setTimeout(() => {
+        setTimeout(() => {
+            button.textContent = "Copier mon email!";
+        }, 1500);
+        button.style.color = "#1A374D";
+        button.classList.remove('mail-btn-clicked');
+    }, 1500);
+}
+
+// Manipulation du DOM
+presentationBox.style.height = (window.innerHeight + 40) + "px";
