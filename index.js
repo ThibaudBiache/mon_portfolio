b0258bed6e0139cc3da02e75be07f6abf419d403
chouka <biache.thibaud@gmail.com>
2022-10-06 10:43:02 +0200

    modif on copy button

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const copyToClipboard = () => {
     navigator.clipboard.writeText("biache.thibaud@gmail.com");
 }
 
-const doSomethingElse = () => {
+const copyButtonStyleModif = () => {
     const button = document.querySelector('.mail-btn');
     console.log(button)
     button.textContent = "Copié !";
     button.classList.add('mail-btn-clicked');
     setTimeout(() => {
         setTimeout(() => {
-            button.textContent = "Copier mon email!";
-        }, 1500);
+            button.textContent = "Copiez mon email!";
+        }, 110);
         button.style.color = "#1A374D";
         button.classList.remove('mail-btn-clicked');
-    }, 1500);
+    }, 800);
 }
 
 // Manipulation du DOM
